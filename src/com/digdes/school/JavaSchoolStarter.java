a8347906e2f0ebfabb060c9d8ed36bfc34130d78
Tural Guluev <tural-98@bk.ru>
2023-03-20 16:51:50 +0300

    the execute method is written in the JavaSchoolStarter class

--- src/com/digdes/school/JavaSchoolStarter.java
+++ src/com/digdes/school/JavaSchoolStarter.java
@@ -5,13 +5,24 @@ import java.util.List;
 import java.util.Map;
 
 public class JavaSchoolStarter {
-    private List<Map<String, Object>> maps = new ArrayList<>();
+    private JavaSchoolStarterServer javaSchoolStarterServer;
 
     public JavaSchoolStarter() {
-
+        javaSchoolStarterServer = new JavaSchoolStarterServer;
     }
 
     public List<Map<String, Object>> execute(String request) throws Exception {
+        switch (request) {
+            case "INSERT":
+                return javaSchoolStarterServer.insert(request);
+            case "UPDATE":
+                return javaSchoolStarterServer.update(request);
+            case "SELECT":
+                return javaSchoolStarterServer.select(request);
+            case "DELETE":
+                return javaSchoolStarterServer.delete(request);
+        }
+
         return new ArrayList<>();
     }
 }
