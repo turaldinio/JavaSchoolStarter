61fefd52db8612c624a355dc523dff3e79da6711
Tural Guluev <tural-98@bk.ru>
2023-04-10 19:04:16 +0300

    added support for all operators

--- src/com/digdes/school/server/ConverterClass.java
+++ src/com/digdes/school/server/ConverterClass.java
@@ -55,7 +55,7 @@ public class ConverterClass {
                         }
 
                         if (requestValueString.matches("%+\\W+%+")) {
-                            String value = requestValueString.substring(requestValueString.indexOf("%")+1, requestValueString.lastIndexOf("%"));
+                            String value = requestValueString.substring(requestValueString.indexOf("%") + 1, requestValueString.lastIndexOf("%"));
                             return currentValueString.contains(value);
                         }
 
@@ -64,18 +64,113 @@ public class ConverterClass {
                             return currentValueString.contains(value);
                         }
                         if (requestValueString.matches("%+\\W+")) {
-                            String value = requestValueString.substring(requestValueString.indexOf("%")+1);
+                            String value = requestValueString.substring(requestValueString.indexOf("%") + 1);
                             return currentValueString.contains(value);
                         }
 
 
+                    }
+                    return false;
+                }
+                , ">", (currentValue, requestValue) -> {
+                    if (currentValue instanceof Long) {
+                        long currentValueLong = (long) currentValue;
+                        long requestValueLong = (long) requestValue;
+                        return currentValueLong > requestValueLong;
+                    }
+                    if (currentValue instanceof Double) {
+                        double currentValueDouble = (double) currentValue;
+                        double requestValueDouble = (double) requestValue;
+                        return currentValueDouble > requestValueDouble;
+                    }
+                    return false;
+                }
+                , "<", (currentValue, requestValue) -> {
+                    if (currentValue instanceof Long) {
+                        long currentValueLong = (long) currentValue;
+                        long requestValueLong = (long) requestValue;
+                        return currentValueLong < requestValueLong;
+                    }
+                    if (currentValue instanceof Double) {
+                        double currentValueDouble = (double) currentValue;
+                        double requestValueDouble = (double) requestValue;
+                        return currentValueDouble < requestValueDouble;
+                    }
+                    return false;
+                }
+                , ">=", (currentValue, requestValue) -> {
+                    if (currentValue instanceof Long) {
+                        long currentValueLong = (long) currentValue;
+                        long requestValueLong = (long) requestValue;
+                        return currentValueLong >= requestValueLong;
+                    }
+                    if (currentValue instanceof Double) {
+                        double currentValueDouble = (double) currentValue;
+                        double requestValueDouble = (double) requestValue;
+                        return currentValueDouble >= requestValueDouble;
+                    }
+                    return false;
+                }
+                , "<=", (currentValue, requestValue) -> {
+                    if (currentValue instanceof Long) {
+                        long currentValueLong = (long) currentValue;
+                        long requestValueLong = (long) requestValue;
+                        return currentValueLong <= requestValueLong;
+                    }
+                    if (currentValue instanceof Double) {
+                        double currentValueDouble = (double) currentValue;
+                        double requestValueDouble = (double) requestValue;
+                        return currentValueDouble <= requestValueDouble;
+                    }
+                    return false;
+                }
+                , "!=",
+                (currentValue, requestValue) -> {
+                    if (currentValue instanceof Double) {
+                        return (double) currentValue != (double) requestValue;
+                    }
+                    if (currentValue instanceof Long) {
+                        return (long) currentValue != (long) requestValue;
+
+                    }
+                    if (currentValue instanceof String) {
+                        return !String.valueOf(currentValue).equals(String.valueOf(requestValue));
+
+                    }
+                    if (currentValue instanceof Boolean) {
+                        return (boolean) currentValue != (boolean) requestValue;
+                    }
+
+
+                    return false;
+                }
+                , "ilike", (currentValue, requestValue) -> {
+                    if (currentValue instanceof String) {
+                        String requestValueString = String.valueOf(requestValue).toLowerCase();
+                        String currentValueString = String.valueOf(currentValue).toLowerCase();
+
+                        if (!requestValueString.contains("%")) {
+                            return currentValueString.contains(requestValueString);
+                        }
+
+                        if (requestValueString.matches("%+\\W+%+")) {
+                            String value = requestValueString.substring(requestValueString.indexOf("%") + 1, requestValueString.lastIndexOf("%"));
+                            return currentValueString.contains(value);
+                        }
+
+                        if (requestValueString.matches("\\W+%+")) {
+                            String value = requestValueString.substring(0, requestValueString.indexOf("%"));
+                            return currentValueString.contains(value);
+                        }
+                        if (requestValueString.matches("%+\\W+")) {
+                            String value = requestValueString.substring(requestValueString.indexOf("%") + 1);
+                            return currentValueString.contains(value);
+                        }
+
 
                     }
                     return false;
-                })
-
-
-        ;
+                });
 
         convertionMap = Map.of(
                 "id", transferToLong,
