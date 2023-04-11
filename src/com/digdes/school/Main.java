c710b6d1573444ab3a40a549418c3fdab9ff8daf
Tural Guluev <tural-98@bk.ru>
2023-04-11 19:49:35 +0300

    SortStation is works

--- src/com/digdes/school/Main.java
+++ src/com/digdes/school/Main.java
@@ -14,7 +14,14 @@ public class Main {
             starter.execute("INSERT VALUES 'lastName' = 'Гулуев ' , 'cost'=1.2, 'id'=1, 'age'=24, 'active'=false");
             starter.execute("INSERT VALUES 'lastName' = 'Мамедов ' , 'cost'=0.3, 'id'=4, 'age'=24, 'active'=true");
             starter.execute("INSERT VALUES 'lastName' = 'Дмитриев ' , 'cost'=23.6, 'id'=2, 'age'=43, 'active'=true");
-            sortStation.calculatePostfixRequest(sortStation.getPostfixRequest("( 'id'>=3 and 'cost'>0 ) or ( 'age'>25 and id>1 )"));
+
+            System.out.println(sortStation.calculatePostfixRequest(
+                    sortStation.getPostfixRequest("'id'=1 or 'cost'=23.6 and 'lastname'like%ев or 'id'>3 ")));
+
+
+
+            // sortStation.calculatePostfixRequest(
+            // sortStation.getPostfixRequest("'id'>=3 and 'cost'>0 or 'lastname'like'%е% "));
 
         } catch (Exception ex) {
             ex.printStackTrace();
--- src/com/digdes/school/SortStation.java
+++ src/com/digdes/school/SortStation.java
@@ -23,7 +23,7 @@ public class SortStation {
         //разбиваем строку пробелами
         String[] values = Objects.requireNonNull(expression).split(" ");
 
-        Stack<String> myStack = new Stack<>();
+        Stack<String> operationStack = new Stack<>();
         StringBuilder stringBuilder = new StringBuilder();
 
         for (String value : values) {
@@ -33,25 +33,24 @@ public class SortStation {
             }
 
             if (value.contains("(")) {
-                myStack.push("(");
+                operationStack.push("(");
                 continue;
             }
 
             if (value.contains(")")) {
 
-                for (int a = 0; a < myStack.size(); a++) {
-                    String as = myStack.peek();
+                for (int a = 0; a < operationStack.size(); a++) {
+                    String as = operationStack.peek();
                     if (!as.equals("(")) {
-                        stringBuilder.append(myStack.pop()).append(" ");
+                        stringBuilder.append(operationStack.pop()).append(" ");
                         a = -1;
                     } else {
-                        myStack.pop();
+                        operationStack.pop();
                         break;
                     }
                 }
                 continue;
             }
-            //where 'id' = 3 and 'lastname' like '%ул%' or 'id'=1
             if (!value.contains("or") && !value.contains("and")) {
                 stringBuilder.append(value).append(" ");
                 continue;
@@ -60,22 +59,19 @@ public class SortStation {
 
             if (value.contains("and") || value.contains("or")) {
                 int currentOperationPriority = operationPriority.get(value);
-                Iterator<String> iterator = myStack.iterator();
-                while (iterator.hasNext()) {
-                    String stackValue = iterator.next();
-                    int priority = operationPriority.get(stackValue);
+                for (int a = 0; a < operationStack.size(); a++) {
+                    int priority = operationPriority.get(operationStack.peek());
                     if (priority >= currentOperationPriority) {
-                        stringBuilder.append(stackValue).append(" ");
-                        iterator.remove();
+                        stringBuilder.append(operationStack.pop()).append(" ");
+                        a -= 1;
                     }
                 }
-                myStack.push(value);
-
+                operationStack.push(value);
             }
-
-
         }
-        for (String s : myStack) {
+
+        Collections.reverse(operationStack);
+        for (String s : operationStack) {
             stringBuilder.append(s).append(" ");
         }
 
@@ -83,7 +79,6 @@ public class SortStation {
     }
 
     public List<Map<String, Object>> calculatePostfixRequest(String postfixLine) {
-        //'id'>=3 'cost'>0 and 'age'>25 id>1 or and
         Stack<String> stack = new Stack<>();
 
         List<Map<String, Object>> list = new ArrayList<>();
@@ -91,22 +86,31 @@ public class SortStation {
 
         var arrayRequest = postfixLine.split(" ");
 
-        for (String value : arrayRequest) {
+        for (int a = 0; a < arrayRequest.length; a++) {
+            String value = arrayRequest[a];
 
             if (!value.contains("or") && !value.contains("and")) {
                 stack.push(value);
                 continue;
             }
+
             if (value.contains("and")) {
+                if (a != arrayRequest.length - 1 && arrayRequest[a + 1].contains("or")) {
+                    String[] array = new String[]{stack.pop(), stack.pop()};
+
+                    saveSuitableCollection(mapStack, array, true, javaSchoolServer.getJavaSchoolRepository().getRepository());
+                    continue;
+                }
+
                 if (mapStack.isEmpty()) {
                     String[] array = new String[]{stack.pop(), stack.pop()};
 
-                    var result = findSuitableCollection(array, true, javaSchoolServer.getJavaSchoolRepository().getRepository());
-                    if (!result.isEmpty()) {
-                        mapStack.push(result);
-                    }
+                    saveSuitableCollection(mapStack, array, true, javaSchoolServer.getJavaSchoolRepository().getRepository());
+
+
                 } else {
-                    String[] array = null;
+                    String[] array = new String[]{stack.pop()};
+
                     var result = findSuitableCollection(array, true, mapStack.pop());
                     if (!result.isEmpty()) {
                         mapStack.push(result);
@@ -117,16 +121,17 @@ public class SortStation {
             }
 
             if (value.contains("or")) {
+                if (stack.isEmpty()) {
+                    break;
+                }
                 if (mapStack.isEmpty()) {
                     String[] array = new String[]{stack.pop(), stack.pop()};
 
-                    var result = findSuitableCollection(array, false, javaSchoolServer.getJavaSchoolRepository().getRepository());
-                    if (!result.isEmpty()) {
-                        mapStack.push(result);
-                    }
+                    saveSuitableCollection(mapStack, array, false, javaSchoolServer.getJavaSchoolRepository().getRepository());
 
                 } else {
                     String[] array = new String[]{stack.pop()};
+
                     var stackList = mapStack.pop();
                     stackList.addAll(findSuitableCollection(array, false, javaSchoolServer.getJavaSchoolRepository().getRepository()));
                     mapStack.push(stackList);
@@ -145,6 +150,13 @@ public class SortStation {
         return list;
 
 
+    }
+
+    public void saveSuitableCollection(Stack<List<Map<String, Object>>> mapStack, String[] array, boolean greedy, List<Map<String, Object>> map) {
+        var result = findSuitableCollection(array, greedy, javaSchoolServer.getJavaSchoolRepository().getRepository());
+        if (!result.isEmpty()) {
+            mapStack.push(result);
+        }
     }
 
 
