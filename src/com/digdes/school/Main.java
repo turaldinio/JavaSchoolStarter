8ad4f665cfef29311da1a6b8e5599e96635fecb7
Tural Guluev <tural-98@bk.ru>
2023-04-12 23:31:59 +0300

    update readme.md

--- src/com/digdes/school/Main.java
+++ src/com/digdes/school/Main.java
@@ -7,13 +7,12 @@ public class Main {
         JavaSchoolStarter starter = new JavaSchoolStarter();
 
         try {
-            starter.execute("INSERT VALUES 'lastName' = 'Гулуев ' , 'cost'=1.2, 'id'=1, 'age'=null, 'active'=false");
-            starter.execute("INSERT VALUES 'lastName' = 'Мамедов ' , 'cost'=32.1, 'id'=2, 'age'=52, 'active'=false");
+            starter.execute("INSERT VALUES 'lastName' = 'Гулуев ' , 'cost'=1.2, 'id'=2, 'age'=null, 'active'=false");
+            starter.execute("INSERT VALUES 'lastName' = 'Мамедов ' , 'cost'=32.1, 'id'=1, 'age'=52, 'active'=false");
             starter.execute("INSERT VALUES 'lastName' = 'Тимуров ' , 'cost'=123.2, 'id'=3, 'age'=13, 'active'=false");
+            var result = starter.execute("UPDATE VALUES 'age'='null,'cost'=12.1 where 'id'>1 and 'lastname'like%ов or id=1 or 'id'=2");
 
-            starter.execute("update values 'active' = true where 'age'>12 or 'age'!=null");
-
-            System.out.println(starter.execute("update values 'age'=null where 'id'>=2"));
+            result.forEach(System.out::println);
 
         } catch (Exception ex) {
             if (ex.getMessage() == null) {
